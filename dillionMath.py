3bbbc874bc183a31fcaa503e8ad9ec3ef58fc45e
BoomBoomMushroom <dillioncat7@gmail.com>
2023-10-11 19:22:01 -0400

    added 3d rendering. crude but still.

--- dillionMath.py
+++ dillionMath.py
@@ -15,7 +15,7 @@ class Vector3:
         return math.sqrt( math.pow(self.x + vecB.x, 2) + math.pow(self.y + vecB.y, 2) + math.pow(self.z + vecB.z, 2) )
 
     def toTuple(self) -> tuple[float, float, float]:
-        return tuple(self.x, self.y, self.z)
+        return (self.x, self.y, self.z)
 
     # Dunder Methods
     def __add__(self, other = None):
@@ -25,23 +25,54 @@ class Vector3:
     def __sub__(self, other = None):
         if type(other) != Vector3: raise TypeError("Can only subtract Vector3 from Vector3")
         return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+    
+    def __mul__(self, other = None):
+        if type(other) == Vector3:
+            return Vector3(self.x * other.x, self.y * other.y, self.z * other.z)
+        elif type(other) == float or type(other) == int:
+            return Vector3(self.x * other, self.y * other, self.z * other)
+        raise TypeError("Can only multiply Vector3 with other Vector3, ints, and floats")
 
     def __str__(self):
         return f"Vector3({self.x}, {self.y}, {self.z})"
 
     # Variables
+    # using "@property" will make it so you can use it like this:
+    # "Vector3.zero" instead of "Vector3.zero()"
+    @property
     def zero():
         return Vector3(0,0,0)
+    @property
     def left():
         return Vector3(-1,0,0)
+    @property
     def right():
         return Vector3(1,0,0)
+    @property
     def up():
         return Vector3(0,1,0)
+    @property
     def down():
         return Vector3(0,-1,0)
+    @property
     def forwards():
         return Vector3(0,0,1)
+    @property
     def back():
         return Vector3(0,0,-1)
 
+def RotationMatrix(alpha, beta, gamma):
+    """
+    rotation matrix of Alpha (α), Beta (β), and Gamma (γ) radians around x, y, z axes (respectively)
+    """
+    sAlpha, cAlpha = math.sin(alpha), math.cos(alpha)
+    sBeta, cBeta = math.sin(beta), math.cos(beta)
+    sGamma, cGamma = math.sin(gamma), math.cos(gamma)
+    return (
+        (cBeta*cGamma, -cBeta*sGamma, sBeta),
+        (cAlpha*sGamma + sAlpha*sBeta*cGamma, cAlpha*cGamma - sGamma*sAlpha*sBeta, -cBeta*sAlpha),
+        (sGamma*sAlpha - cAlpha*sBeta*cGamma, cAlpha*sGamma*sBeta + sAlpha*cGamma, cAlpha*cBeta)
+    )
+    
+    # From https://stackoverflow.com/questions/21019471/how-can-i-draw-a-3d-shape-using-pygame-no-other-modules
+    # Answer by Nitsan BenHanoch
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 # Custom Imports
-from dillionMath import Vector3
-
+from dillionMath import Vector3, RotationMatrix
+from pygame_utils.warp import warp as ImageWarpPygame
 
 # Imports
 import pygame
+import numpy as np
+import random
+import pywavefront
 
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
-camera_pos: tuple = (0, 0, 0)
+#screen = pygame.display.set_mode((450,450))
+camera_position: tuple = (0, 0, 0)
+
+WHITE_COLOR = (255,255,255,255)
 
 class Point():
-    def __init__(self, pos: tuple[float, float, float], color: tuple[int, int, int, int] = (255, 255, 255, 255)) -> None:
-        self.pos = pos
+    def __init__(self, position: tuple[float, float, float], color: tuple[int, int, int, int] = (255, 255, 255, 255)) -> None:
+        if type(position) == Vector3: position = position.toTuple()
+        
+        self.position = position
         self.color = color
 
     def tick(self) -> None:
@@ -34,15 +42,75 @@ class Wireframe():
         for i in self.edges:
             i.tick()
 
+
+
+
+
+class Object():
+    def __init__(self, vertices: list[Vector3] = None, edges: list[tuple[int, int]] = None) -> None:
+        if vertices == None or edges == None:
+            vertices = [(1, 1, 1), (1, 1, -1), (1, -1, 1), (1, -1, -1), (-1, 1, 1), (-1, 1, -1), (-1, -1, 1), (-1, -1, -1)]
+            edges = [(0, 1), (0, 2), (2, 3), (1, 3), (4, 5), (4, 6), (6, 7), (5, 7), (0, 4), (1, 5), (2, 6), (3, 7)]
+        
+        self.vertices = np.array(vertices)
+        self.edges = edges
+        self.rotation = [0, 0, 0]
+        
+        self.randomEdge = []
+        for i in range(len(edges)):
+            self.randomEdge.append((
+                random.randint(0,len(vertices)-1),
+                random.randint(0,len(vertices)-1)
+            ))
+    
+    def tick(self):
+        r = 0.05 / 25
+        
+        self.rotation[0] += r
+        self.rotation[1] += r/2
+        self.rotation[2] += r/3
+    
+    def fit(self, vec):
+        return [round(70 * coordinate + frame / 2) for coordinate, frame in zip(vec, screen.get_size())]
+    
+    def draw(self):
+        location = self.vertices.dot(RotationMatrix(*self.rotation))  # an index->location mapping
+        out = ((location[v1], location[v2]) for v1, v2 in self.edges)
+        
+        outList = []
+        
+        for start, end in out:
+            a = self.fit(start)
+            b = self.fit(end)
+            
+            outList.append(a)
+            outList.append(b)
+            pygame.draw.line(screen, (255, 128, 128), a, b, 4)
+        
+        
+
+
 wireframes: list[Wireframe] = []
 
-wireframes.append(Wireframe([
-
-]))
+obj = Object(
+    [(-1,-1,-1), (-1,-1,1), (-1,1,-1), (-1,1,1),   (1,-1,-1), (1,-1,1), (1,1,-1), (1,1,1)],
+    [(0,1),(0,2),(1,3),(2,3),  (0,4),(1,5),(2,6),(3,7),  (4,5),(4,6),(5,7),(6,7)],
+)
 
 while True:
+    screen.fill((0,0,0))
+     
     for event in pygame.event.get():
         if event == pygame.QUIT:
             pygame.quit()
+    
+    """
     for i in wireframes:
         i.tick()
+    """
+    
+    
+    obj.draw()
+    obj.tick()
+    
+    pygame.display.update()
